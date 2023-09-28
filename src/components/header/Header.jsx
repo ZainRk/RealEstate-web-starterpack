2aaa5520d56f915ac941e835e3fedbde431aac93
Hadi Allahverdi Pournigjeh <108987953+hadipournigjeh@users.noreply.github.com>
2023-09-28 23:29:33 +0100

    Filled all a tags

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,11 +7,21 @@ const Header = () => {
         <img src="./logo.png" alt="Logo" width={100} />
 
         <div class="h-menu">
-          <a href="" class=""></a>
-          <a href="" class=""></a>
-          <a href="" class=""></a>
-          <a href="" class=""></a>
-          <a href="" class=""></a>
+          <a href="" class="">
+            Residencies
+          </a>
+          <a href="" class="">
+            Our Value
+          </a>
+          <a href="" class="">
+            Contact Us
+          </a>
+          <a href="" class="">
+            Get Started
+          </a>
+          <a href="" class="">
+            Contact
+          </a>
         </div>
       </div>
     </section>
