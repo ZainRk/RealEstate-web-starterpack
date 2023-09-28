01419b70a91d0af787cec77800c866461d64f969
Hadi Allahverdi Pournigjeh <108987953+hadipournigjeh@users.noreply.github.com>
2023-09-28 23:25:07 +0100

    Created image tag for logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Header = () => {
-  return <section class="h-wrapper"></section>;
+  return (
+    <section class="h-wrapper">
+      <div class="h-container">
+        <img src="./logo.png" alt="Logo" />
+      </div>
+    </section>
+  );
 };
 
 export default Header;
