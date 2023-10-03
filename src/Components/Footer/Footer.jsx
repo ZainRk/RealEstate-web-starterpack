eafba6a6cb716ff94a62b8e938ac0dd7102d5ea4
kyl-136 <kennethye2004@gmail.com>
2023-10-02 20:21:13 -0400

    Update Footer.jsx
    deny this

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <section>
+        <div>
+            <h1>Footer</h1>
+        </div>
+        <div>
+            <h2>
+                Hello
+            </h2>
+        </div>
+        </section>
+        
+    );
+};
+
+
+export default Footer;
