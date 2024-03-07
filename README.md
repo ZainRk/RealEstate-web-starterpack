51b1ff73d7d92e99710af6cb71763ec671a1f763
Hoshi Holiday <hoshiholiday@gmail.com>
2024-03-07 15:24:02 -0600

    beginning contact form
    contact form begins

---
+++ README.md
@@ -0,0 +1,2 @@
+# Gift_of_Faith_v2.0
+ Gift of Faith part 2
--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -5,9 +5,7 @@ import * as bootstrap from 'bootstrap'
 
 const Team = () => {
   return (
- 
     <div>
-
       <h1>Meet the Team!</h1>
       <div className='super-container'>
       <div className="row">
@@ -27,23 +25,66 @@ const Team = () => {
       <br/>
       <br/>
 
-      <div className='super-container-b'>
+      <div className='super-container'>
       <div className="row">
-      <div className="container-b">
+      <div className="image-section-c">
       <div className="text-section">
           <h2>Column 2</h2>
         <p>Some text..</p>
         </div>
         </div>
-      <div className="image-section-b">
+      <div className="container-b">
+      <div className="text-section">
+      <img className="card-img-top" src="./businesswoman.jpeg" alt="" />
+          <h2>Column 2</h2>
+        <p>Some text..</p>
+        </div>
+        </div>
+       </div>
+       
+      </div>
+      <br/>
+      <br/>
+      <div className='super-container'>
+      <div className="row">
+      <div className="container">
+      <div className="text-section">
+          <h2>Column 2</h2>
+        <p>Some text..</p>
+        </div>
+        </div>
+          <div className="image-section">
             <img className="card-img-top" src="./businesswoman.jpeg" alt="" />
             <h2>Column 1</h2>
             <p>Some text..</p>
           </div>
-
        </div>
       </div>
       <br/>
+      <br/>
+
+      <div className='super-container'>
+      <div className="row">
+      <div className="image-section-c">
+      <div className="text-section">
+          <h2>Column 2</h2>
+        <p>Some text..</p>
+        </div>
+        </div>
+      <div className="container-b">
+      <div className="text-section">
+      <img className="card-img-top" src="./businesswoman.jpeg" alt="" />
+          <h2>Column 2</h2>
+        <p>Some text..</p>
+        </div>
+        </div>
+       </div>
+       
+      </div>
+      <br/>
+      <br/>
+
+
     </div> 
   )
 }
